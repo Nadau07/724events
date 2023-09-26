14fd8bcd7ace91323902ad921dc3681523ca72ea
Nadau07 <noah.dev7@gmail.com>
2023-09-26 11:00:09 +0200

    Suppression de l'avertissement de la console (slider)

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -30,41 +30,42 @@ const Slider = () => {
   });
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+    {byDateDesc?.map((event, idx) => (
+      <div key={event.title}>
+        <div
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                /* une key doit être unique : modif de la clef ci-dessous */
-                  key={`${_.title}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  readOnly
-                  /* Pour que les bullets suivent le carousel, remplacement de idx par index (verifie quel bouton radio doit etre coché) */
-                />
-              ))}
-            </div>
+        </div>
+        <div className="SlideCard__paginationContainer">
+          <div className="SlideCard__pagination">
+            {/* Changement de _ par dot (bouton) : meilleur comprehension */}
+            {byDateDesc.map((dot, radioIdx) => (
+              <input
+                /* Changement de la key {`${event.id}`} , elle doit être unique */
+                key={`${dot.title}`}
+                type="radio"
+                name="radio-button"
+                /* Pour que les bullets suivent le carousel, remplacement de idx par index (verifie quel bullet doit être coché) */
+                checked={index === radioIdx}
+                readOnly
+              />
+            ))}
           </div>
-        </>
-      ))}
-    </div>
-  );
+        </div>
+      </div>
+    ))}
+  </div>
+);
 };
 
 export default Slider;
